ab84ca5d828d43cdf965df15049d1e2946fdd373
missacodes <issam337b@gmail.com>
2026-04-17 23:21:25 +0100

    added the data structure for all 12 world cup groups

---
+++ script.js
@@ -0,0 +1,40 @@
+
+const teams = [
+        {
+            group: "Group A", name: ["Mexico", "South Africa", "South Korea", "Czech Republic"], images:[".../images/a_group/mexico.png", ".../images/a_group/southafrica.png", ".../images/a_group/southkorea.png", ".../images/a_group/czechia.png"]
+        },
+        {
+            group: "Group B", name: ["Canada", "Bosnia", "Qatar", "Switzerland"], images:[".../images/b_group/canada.png", ".../images/b_group/bosnia.png", ".../images/b_group/qatar.png", ".../images/b_group/switzerland.png"]
+         },
+        {
+            group: "Group C", name: ["Brazil", "Ghana", "Morocco", "Scotland"], images:[".../images/c_group/brazil.png", ".../images/c_group/morocco.png", ".../images/c_group/haiti.png", ".../images/c_group/scotland.png"]
+        },
+        {
+            group: "Group D", name: ["USA", "Paraguay", "Australia", "Turkey"], images:[".../images/d_group/brazil.png", ".../images/d_group/morocco.png", ".../images/d_group/haiti.png", ".../images/d_group/scotland.png"]
+        },
+        {
+            group: "Group E", name: ["Germany", "Curacao", "Ivory Coast", "Ecuador"], images:[".../images/e_group/germany.png", ".../images/e_group/curacao.png", ".../images/e_group/ivorycoast.png", ".../images/e_group/ecuador.png"]
+        },
+        {
+            group: "Group F", name: ["Netherlands", "Japan", "Sweden", "Tunisia"], images:[".../images/f_group/netherlands.png", ".../images/f_group/japan.png", ".../images/f_group/sweden.png", ".../images/f_group/tunisia.png"]
+        },
+        {
+            group: "Group G", name: ["Belgium", "Egypt", "Iran", "New Zealand"], images:[".../images/g_group/belgium.png", ".../images/g_group/egypt.png", ".../images/g_group/iran.png", ".../images/g_group/newzealand.png"]
+        },
+        {
+            group: "Group H", name: ["Spain", "Cape Verde", "Saudi Arabia", "Uruguay"], images:[".../images/h_group/spain.png", ".../images/h_group/capeverde.png", ".../images/h_group/saudiarabia.png", ".../images/h_group/uruguay.png"]
+        },
+        {
+            group: "Group I", name: ["France", "Senegal", "Iraq", "Norway"], images:[".../images/i_group/france.png", ".../images/i_group/senegal.png", ".../images/i_group/iraq.png", ".../images/i_group/norway.png"]
+        },
+        {
+            group: "Group J", name: ["Argentina", "Algeria", "Austria", "Jordan"], images:[".../images/j_group/argentina.png", ".../images/j_group/algeria.png", ".../images/j_group/austria.png", ".../images/j_group/jordan.png"]
+        },
+        {
+            group: "Group K", name: ["Portugal", "DR Congo", "Uzbekistan", "Colombia"], images:[".../images/k_group/portugal.png", ".../images/k_group/drcongo.png", ".../images/k_group/uzbekistan.png", ".../images/k_group/colombia.png"]
+        },
+        {
+            group: "Group L", name: ["England", "Croatia", "Ghana", "Panama"], images:[".../images/l_group/england.png", ".../images/l_group/croatia.png", ".../images/l_group/ghana.png", ".../images/l_group/panama.png"]
+        }
+
+]
