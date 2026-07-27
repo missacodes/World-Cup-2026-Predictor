05c6ae1ac5fcacf64adc26c09302b93b2c23914a
missacodes <issam337b@gmail.com>
2026-07-27 21:56:47 +0100

    website is complete

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const teams = [
             group: "Group B", name: ["Canada", "Bosnia", "Qatar", "Switzerland"], images:["images/b_group/canada.png", "images/b_group/bosnia.png", "images/b_group/qatar.png", "images/b_group/switzerland.png"]
          },
         {
-            group: "Group C", name: ["Brazil", "Haiti", "Morocco", "Scotland"], images:["images/c_group/brazil.png", "images/c_group/morocco.png", "images/c_group/haiti.png", "images/c_group/scotland.png"]
+            group: "Group C", name: ["Brazil", "Haiti", "Morocco", "Scotland"], images:["images/c_group/brazil.png", "images/c_group/haiti.png", "images/c_group/morocco.png", "images/c_group/scotland.png"]
         },
         {
             group: "Group D", name: ["USA", "Paraguay", "Australia", "Turkey"], images:["images/d_group/usa.png", "images/d_group/paraguay.png", "images/d_group/australia.png", "images/d_group/turkey.png"]
@@ -39,83 +39,141 @@ const teams = [
 
 ]
 
-const rankings = [[], [], [], [], [], [], [], [], [], [], [], []]
+const rankings = []
+for (let i = 0; i < teams.length; i++) {
+    rankings.push(teams[i].name.slice())
+}
 const third = []
+const teamImages = {}
+for (let i = 0; i < teams.length; i++) {
+    for (let j = 0; j < teams[i].name.length; j++) {
+        teamImages[teams[i].name[j]] = teams[i].images[j]
+    }
+}
+const rankBorders = ["2px solid gold", "2px solid silver", "2px solid #CD7F32", "2px solid red"]
+let dragSource = null
 const groupStage = document.getElementById("group-stage")
 let html = "";
 for (let i = 0; i < teams.length; i++) {
     html += "<div class='group-card'>"
     html += "<h2>" + teams[i].group + "</h2>"
-    html += "<ul id='group-" + i + "'>";
-    for (let j = 0; j<teams[i].name.length; j++) {
-        html += "<li id='team-" + teams[i].name[j] + "'><img src='" + teams[i].images[j] + "'>" + teams[i].name[j] + "<button onclick='pickTeam(\"" + teams[i].name[j] + "\", " + i + ")'> Pick </button></li>"
-
-    }
-    html += "</ul>";
+    html += "<ul id='group-" + i + "'></ul>";
     html += "</div>";
 }
 groupStage.innerHTML = html;
-function pickTeam(teamName, groupIndex) {
-    console.log( teamName, groupIndex )
-    const groupList = document.getElementById("group-" + groupIndex)
-    console.log(groupList)
-    if (rankings[groupIndex].length<4 && !(rankings[groupIndex].includes(teamName))) {
-        rankings[groupIndex].push(teamName)
+for (let i = 0; i < teams.length; i++) {
+    renderGroup(i)
+}
+function renderGroup (groupIndex) {
+    let groupHtml = ""
+    for (let j = 0; j < rankings[groupIndex].length; j++) {
+        const teamName = rankings[groupIndex][j]
+        groupHtml += "<li id='team-" + groupIndex + "-" + j + "' draggable='true' style='border: " + rankBorders[j] + "'"
+            + " ondragstart='dragStart(event, " + groupIndex + ", " + j + ")'"
+            + " ondragover='allowDrop(event)'"
+            + " ondrop='dropTeam(event, " + groupIndex + ", " + j + ")'>"
+            + "<img src='" + teamImages[teamName] + "'>" + teamName
+            + "</li>"
     }
-    console.log(rankings)
-    const teamList = document.getElementById("team-" + teamName)
-    if (rankings[groupIndex][0] === teamName) {
-    teamList.style.border = "2px solid gold"
+    document.getElementById("group-" + groupIndex).innerHTML = groupHtml
+}
+function dragStart (event, groupIndex, rankIndex) {
+    dragSource = { groupIndex: groupIndex, rankIndex: rankIndex }
+}
+function allowDrop (event) {
+    event.preventDefault()
+}
+function dropTeam (event, groupIndex, rankIndex) {
+    event.preventDefault()
+    if (dragSource !== null && dragSource.groupIndex === groupIndex && dragSource.rankIndex !== rankIndex) {
+        const swap = rankings[groupIndex][dragSource.rankIndex]
+        rankings[groupIndex][dragSource.rankIndex] = rankings[groupIndex][rankIndex]
+        rankings[groupIndex][rankIndex] = swap
+        renderGroup(groupIndex)
     }
-    if (rankings[groupIndex][1] === teamName) {
-        teamList.style.border = "2px solid silver"
-    }
-    if (rankings[groupIndex][2] === teamName) {
-        teamList.style.border = "2px solid #CD7F32"
-    }
-    if (rankings[groupIndex].length === 3) {
-        const lastTeam = teams[groupIndex].name.find(function(team) {
-            return !(rankings[groupIndex].includes(team))
-        })
-        const lastTeamElement = document.getElementById("team-" + lastTeam)
-        lastTeamElement.style.border = "2px solid red"
-    }
-    const allGroupsDone = rankings.every(function(group) {
-        return group.length >= 2
-    })
-    const allThirdDone = rankings.every(function(group) {
-        return group.length >= 3
-    })
-
-    if (allThirdDone) {
-        pickThird ()
-    }
-
+    dragSource = null
 }
 const knockoutStage = document.getElementById("knockout-stage")
+let currentRound = {}
 function roundOf32 () {
-    knockoutStage.innerHTML = "<h2>Round of 32</h2>" + "<br>"
-        + "<p>" + rankings[0][1] + " vs " + rankings[1][1] + "</p>"
-        + "<p>" + rankings[5][0] + " vs " + rankings[2][1] + "</p>"
-        + "<p>" + rankings[2][0] + " vs " + rankings[5][1] + "</p>"
-        + "<p>" + rankings[4][1] + " vs " + rankings[8][1] + "</p>"
-        + "<p>" + rankings[10][1] + " vs " + rankings[11][1] + "</p>"
-        + "<p>" + rankings[7][0] + " vs " + rankings[9][1] + "</p>"
-        + "<p>" + rankings[9][0] + " vs " + rankings[7][1] + "</p>"
-        + "<p>" + rankings[3][1] + " vs " + rankings[6][1] + "</p>"
-        + "<p>" + rankings[4][0] + " vs " + third[0] + "</p>"
-        + "<p>" + rankings[8][0] + " vs " + third[1] + "</p>"
-        + "<p>" + rankings[0][0] + " vs " + third[2] + "</p>"
-        + "<p>" + rankings[11][0] + " vs " + third[3] + "</p>"
-        + "<p>" + rankings[3][0] + " vs " + third[4] + "</p>"
-        + "<p>" + rankings[6][0] + " vs " + third[5] + "</p>"
-        + "<p>" + rankings[1][0] + " vs " + third[6] + "</p>"
-        + "<p>" + rankings[10][0] + " vs " + third[7] + "</p>"
+    const matches = [
+        [rankings[0][1], rankings[1][1]],
+        [rankings[5][0], rankings[2][1]],
+        [rankings[2][0], rankings[5][1]],
+        [rankings[4][1], rankings[8][1]],
+        [rankings[10][1], rankings[11][1]],
+        [rankings[7][0], rankings[9][1]],
+        [rankings[9][0], rankings[7][1]],
+        [rankings[3][1], rankings[6][1]],
+        [rankings[4][0], third[0]],
+        [rankings[8][0], third[1]],
+        [rankings[0][0], third[2]],
+        [rankings[11][0], third[3]],
+        [rankings[3][0], third[4]],
+        [rankings[6][0], third[5]],
+        [rankings[1][0], third[6]],
+        [rankings[10][0], third[7]]
+    ]
+    startRound(matches, "Round of 32", roundOf16)
+}
+function roundOf16 (matches) {
+    startRound(matches, "Round of 16", quarterFinals)
+}
+function quarterFinals (matches) {
+    startRound(matches, "Quarterfinals", semiFinals)
+}
+function semiFinals (matches) {
+    startRound(matches, "Semifinals", finalRound)
+}
+function finalRound (matches) {
+    startRound(matches, "Final", null)
+}
+function startRound (matches, roundName, nextRound) {
+    currentRound = { matches: matches, winners: new Array(matches.length).fill(null), name: roundName, next: nextRound }
+    let roundHtml = "<h2>" + roundName + "</h2>" + "<br>" + "<ul>"
+    for (let i = 0; i < matches.length; i++) {
+        roundHtml += "<li id='match-" + i + "'>"
+            + "<img src='" + teamImages[matches[i][0]] + "'>"
+            + "<span id='pick-" + matches[i][0] + "'>" + matches[i][0] + "</span>"
+            + "<button onclick='pickWinner(\"" + matches[i][0] + "\", " + i + ")'> Pick </button>"
+            + "<span> vs </span>"
+            + "<img src='" + teamImages[matches[i][1]] + "'>"
+            + "<span id='pick-" + matches[i][1] + "'>" + matches[i][1] + "</span>"
+            + "<button onclick='pickWinner(\"" + matches[i][1] + "\", " + i + ")'> Pick </button>"
+            + "</li>"
+    }
+    roundHtml += "</ul>"
+    knockoutStage.innerHTML = roundHtml
+}
+function pickWinner (teamName, matchIndex) {
+    if (currentRound.winners[matchIndex] === null) {
+        currentRound.winners[matchIndex] = teamName
+        const pickedElement = document.getElementById("pick-" + teamName)
+        pickedElement.style.border = "2px solid gold"
+    }
+    const allPicked = currentRound.winners.every(function(winner) {
+        return winner !== null
+    })
+    if (allPicked) {
+        if (currentRound.winners.length === 1) {
+            showChampion(currentRound.winners[0])
+        } else {
+            const nextMatches = []
+            for (let i = 0; i < currentRound.winners.length; i += 2) {
+                nextMatches.push([currentRound.winners[i], currentRound.winners[i + 1]])
+            }
+            currentRound.next(nextMatches)
+        }
+    }
+}
+function showChampion (teamName) {
+    knockoutStage.innerHTML += "<h2>Champion</h2>"
+        + "<p class='champion'><img src='" + teamImages[teamName] + "'> " + teamName + "</p>"
 }
 function pickThird () {
     let html3 = "<h2>Pick the 8 best 3rd placed teams</h2>"
         for (let i = 0; i < rankings.length; i++) {
-            html3 += "<li id='third-" + rankings[i][2] + "'><img src='" + teams[i].images[2] + "'>" + rankings[i][2] + "<button onclick='selectThird(\"" + rankings[i][2] + "\")'> Pick </button></li>"
+            html3 += "<li id='third-" + rankings[i][2] + "'><img src='" + teamImages[rankings[i][2]] + "'>" + rankings[i][2] + "<button onclick='selectThird(\"" + rankings[i][2] + "\")'> Pick </button></li>"
         }
         knockoutStage.innerHTML = html3;
 }
